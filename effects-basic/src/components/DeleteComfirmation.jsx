163553a8bba8b7355d302b29a7f525fc85796bd9
myky034 <vumyky.034@gmail.com>
2024-02-21 20:35:20 +0700

    Finished project Effect Basic with using useEffect, useCallBack

--- effects-basic/src/components/DeleteComfirmation.jsx
+++ effects-basic/src/components/DeleteComfirmation.jsx
@@ -1,4 +1,21 @@
+import { useEffect } from "react";
+import ProgressBar from "./ProgressBar";
+
+const TIMER = 3000;
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    console.log("TIMER SET");
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, TIMER);
+
+    return () => {
+      console.log("Cleaning up timer");
+      clearTimeout(timer);
+    };
+  }, [onConfirm]);
+
   return (
     <div id='delete-confirmation'>
       <h2>Are you sure?</h2>
@@ -14,6 +31,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           className='button'>
           Yes
         </button>
+        <ProgressBar timer={TIMER} />
       </div>
     </div>
   );
